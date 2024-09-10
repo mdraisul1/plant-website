ca42d4f340b28b87c9c002db56b131e2ca130074
Raisul Islam <mdraju3248@gmail.com>
2024-09-11 01:47:25 +0600

    Added hero section blob design

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,35 @@ module.exports = {
       '2xl': '1440px',
     },
     extend: {},
+    keyframes: {
+      move:{
+        "50%":{transform:"translateY(-1rem)"}
+      },
+      rotate: {
+        '0%': { transform: 'rotate(0deg)' },
+        '100%': { transform: 'rotate(360deg)' },
+      },
+      scale: {
+        '0%': { transform: 'scale(0.8)' },
+        '50%': { transform: 'scale(1.2)' },
+        '100%': { transform: 'scale(0.8)' },
+      },
+      float: {
+        '0%, 100%': { transform: 'translateY(0)' },
+        '50%': { transform: 'translateY(-1rem)' },
+      },
+      floatReverse: {
+        '0%, 100%': { transform: 'translateY(0)' },
+        '50%': { transform: 'translateY(1rem)' },
+      },
+    },
+    animation: {
+      moveingY: 'move 4s ease-in-out infinite',
+      rotate: 'rotate 15s linear infinite',
+      scale: 'scale 15s linear infinite',
+      float: 'float 5s ease-in-out infinite',
+      floatReverse: 'floatReverse 5s ease-in-out infinite',
+    },
     fontFamily: {
       Jost: ['Jost', 'sans-serif'],
       Lobster : ["Lobster", "sans-serif"],
