f251633aebea016e06b0530ec805a7dfec37448d
Raisul Islam <mdraju3248@gmail.com>
2024-09-10 17:51:00 +0600

    Added mobile hamdurger menu toggle function

---
+++ main.js
@@ -0,0 +1,9 @@
+const navMenu = document.getElementById("nav-menu")
+const navLink = document.getElementById("nav-link")
+const hamburger = document.getElementById("hamburger")
+
+hamburger.addEventListener("click", () => {
+    navMenu.classList.toggle("left-[0]")
+})
+
+
