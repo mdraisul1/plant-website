70872987da12eb09b214e2612b1eb3ede4ba3323
Raisul Islam <mdraju3248@gmail.com>
2024-09-10 23:58:52 +0600

    Added hero section layout

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const navMenu = document.getElementById("nav-menu")
-const navLink = document.getElementById("nav-link")
+const navLink = document.querySelectorAll(".nav-link")
 const hamburger = document.getElementById("hamburger")
 
 hamburger.addEventListener("click", () => {
@@ -7,4 +7,9 @@ hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("ri-close-large-line")
 })
 
-
+navLink.forEach(link => {
+    link.addEventListener("click", () => {
+        navMenu.classList.toggle("left-[0]")
+        hamburger.classList.toggle("ri-close-large-line")
+    })
+});
